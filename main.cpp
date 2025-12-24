2e5f014bbc3330a7a7672bf08d86cd3b89a144a1
sscg13 <iamdumbybear1@gmail.com>
2025-12-23 18:37:35 -0800

    TT + improve hand reading

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <iostream>
+#include <random>
 #include <sstream>
 #include <string>
 
@@ -7,8 +8,13 @@ using U64 = uint64_t;
 
 constexpr int SCORE_INF = 2000;
 constexpr int SCORE_MATE = 1900;
+constexpr int SCORE_MAX_EVAL = 1800;
 constexpr int MAX_PLY = 64;
 constexpr int MAX_MOVES = 64;
+constexpr int TT_SIZE = 65536;
+constexpr int EXPECTED_PV_NODE = 3;
+constexpr int EXPECTED_ALL_NODE = 2;
+constexpr int EXPECTED_CUT_NODE = 1;
 
 int move(int rank, int count) {
     return 16 * count + rank;
@@ -19,11 +25,34 @@ int rank(int play) {
 int count(int play) {
     return play / 16;
 }
-
+U64 cardhashes[2][16][5];
+U64 movehashes[96];
+U64 colorhash;
+void initializezobrist() {
+    std::mt19937_64 mt(20346892);
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < 5; j++) {
+            cardhashes[0][i][j] = mt();
+            cardhashes[1][i][j] = mt();
+        }
+    }
+    for (int i = 0; i < 96; i++) {
+        movehashes[i] = mt();
+    }
+    colorhash = mt();
+    for (int j = 0; j < 5; j++) {
+        cardhashes[0][15][j] = (U64)0;
+        cardhashes[1][15][j] = (U64)0;
+    }
+}
 struct Board {
     int totals[2][2];
     int counts[2][16];
     int color = 0;
+    U64 deckhashes[2];
+    U64 hash(int play);
+    U64 hashafter(int play);
+    bool islegal(int curr, int next);
     void makemove(int play);
     void unmakemove(int play);
     int generatemoves(int play, int *movelist);
@@ -40,6 +69,15 @@ struct Limits {
 struct Options {
     bool suppressoutput = false;
 };
+struct TTentry {
+    U64 data;
+    void update(U64 hash, int depth, int ply, int score, int nodetype, int hashmove);
+    U64 key();
+    int hashmove();
+    int depth();
+    int score(int ply);
+    int nodetype();
+};
 struct Searcher {
     Board decks;
     Limits searchlimits;
@@ -49,18 +87,46 @@ struct Searcher {
     int lastmove;
     bool stopsearch;
     std::chrono::time_point<std::chrono::steady_clock> start;
+    TTentry TT[TT_SIZE];
     int alphabeta(int depth, int ply, int alpha, int beta, int play);
     int iterative(int play);
     void reset();
     void interface();
 };
+U64 Board::hash(int play) {
+    U64 hash = deckhashes[0] ^ deckhashes[1] ^ movehashes[play];
+    if (color) {
+        hash ^= colorhash;
+    }
+    return hash;
+}
+bool Board::islegal(int curr, int next) {
+    if (count(curr) == 0) {
+        return true;
+    }
+    else {
+        if (count(next) != count(curr)) {
+            return false;
+        }
+        if (rank(next) <= rank(curr)) {
+            return false;
+        }
+        if (counts[color][rank(next)] < count(next)) {
+            return false;
+        }
+        return true;
+    }
+}
 void Board::makemove(int play) {
+    deckhashes[color] ^= cardhashes[color][rank(play)][counts[color][rank(play)]];
+    counts[color][rank(play)] -= count(play);
     totals[color][0] -= count(play);
     totals[color][1] -= (128 * count(play) * rank(play));
-    counts[color][rank(play)] -= count(play);
+    deckhashes[color] ^= cardhashes[color][rank(play)][counts[color][rank(play)]];
     if (rank(play) == 15) {
         counts[color][14]--;
         counts[color][13]--;
+        deckhashes[color] ^= (cardhashes[color][14][1] ^ cardhashes[color][13][1]);
     }
     else if (rank(play) >= 13) {
         counts[color][15]--;
@@ -69,12 +135,15 @@ void Board::makemove(int play) {
 }
 void Board::unmakemove(int play) {
     color ^= 1;
+    deckhashes[color] ^= cardhashes[color][rank(play)][counts[color][rank(play)]];
+    counts[color][rank(play)] += count(play);
     totals[color][0] += count(play);
     totals[color][1] += (128 * count(play) * rank(play));
-    counts[color][rank(play)] += count(play);
+    deckhashes[color] ^= cardhashes[color][rank(play)][counts[color][rank(play)]];
     if (rank(play) == 15) {
         counts[color][14]++;
         counts[color][13]++;
+        deckhashes[color] ^= (cardhashes[color][14][1] ^ cardhashes[color][13][1]);
     }
     else if (rank(play) >= 13) {
         counts[color][15]++;
@@ -111,13 +180,48 @@ int Board::evaluate() {
 void Board::set(int side, std::string deck) {
     totals[side][0] = 0;
     totals[side][1] = 0;
-    for (int i = 0; i < 15; i++) {
-        counts[side][i] = (deck[i] - '0');
-        totals[side][0] += counts[side][i];
-        totals[side][1] += 128 * counts[side][i] * i;
+    int i = 0;
+    int j = 0;
+    deckhashes[side] = (U64)0;
+    while (i < 15) {
+        if (deck[j] >= '0' && deck[j] <= '4') {
+            counts[side][i] = (deck[j] - '0');
+            totals[side][0] += counts[side][i];
+            totals[side][1] += 128 * counts[side][i] * i;
+            deckhashes[side] ^= cardhashes[side][i][counts[side][i]];
+            i++;
+        }
+        j++;
     }
     counts[side][15] = counts[side][13] + counts[side][14];
 }
+void TTentry::update(U64 hash, int depth, int ply, int score, int nodetype, int hashmove) {
+  data = (hash >> 32);
+  if (score > SCORE_MAX_EVAL) {
+    score += ply;
+  }
+  if (score < -SCORE_MAX_EVAL) {
+    score -= ply;
+  }
+  data |= ((U64)((unsigned short int)score) << 32);
+  data |= (((U64)hashmove) << 43);
+  data |= (((U64)nodetype) << 51);
+  data |= (((U64)depth) << 53);
+}
+U64 TTentry::key() { return (data & 0xFFFFFFFF); }
+int TTentry::hashmove() { return (int)(data >> 43) & 0xFF; }
+int TTentry::depth() { return (int)(data >> 53) & 63; }
+int TTentry::score(int ply) {
+  int score = (int)(short int)((data >> 32) & 0x00000000000007FF);
+  if (score > SCORE_MAX_EVAL) {
+    return score - ply;
+  }
+  if (score < -SCORE_MAX_EVAL) {
+    return score + ply;
+  }
+  return score;
+}
+int TTentry::nodetype() { return (int)(data >> 51) & 3; }
 int Searcher::alphabeta(int depth, int ply, int alpha, int beta, int play) {
     pvtable[ply][0] = ply + 1;
     if (depth <= 0 || ply >= searchlimits.maxdepth) {
@@ -125,8 +229,38 @@ int Searcher::alphabeta(int depth, int ply, int alpha, int beta, int play) {
     }
     int score = -SCORE_INF;
     int bestscore = -SCORE_INF;
+    bool improvedalpha = false;
+    U64 currhash = decks.hash(play);
+    int index = decks.hash(play) % TT_SIZE;
+    int ttmove = -1;
+    TTentry &ttentry = TT[index];
+    bool tthit = (ttentry.key() == (currhash >> 32));
+    bool isPV = (beta - alpha) > 1;
+    int ttdepth = ttentry.depth();
+    bool update = (depth >= ttdepth || !tthit);
+    if (tthit) {
+        score = ttentry.score(ply);
+        ttmove = ttentry.hashmove();
+        /*int ttnodetype = ttentry.nodetype();
+        if (!isPV && ttdepth >= depth) {
+            if (ttnodetype == EXPECTED_PV_NODE) {
+                return score;
+            }
+            if ((ttnodetype & EXPECTED_CUT_NODE) && (score >= beta)) {
+                return score;
+            }
+            if ((ttnodetype & EXPECTED_ALL_NODE) && (score <= alpha)) {
+                return score;
+            }
+        }*/
+    }
     int moves[MAX_MOVES];
     int movcount = decks.generatemoves(play, moves);
+    for (int i = 0; i < movcount; i++) {
+        if (moves[i] == ttmove) {
+            std::swap(moves[i], moves[0]);
+        }
+    }
     for (int i = 0; i < movcount; i++) {
         bool nullwindow = (i > 0);
         int e = (movcount == 1);
@@ -151,9 +285,13 @@ int Searcher::alphabeta(int depth, int ply, int alpha, int beta, int play) {
             if (score > bestscore) {
                 if (score > alpha) {
                     if (score >= beta) {
+                        if (update && !stopsearch) {
+                            ttentry.update(currhash, depth, ply, score, EXPECTED_CUT_NODE, mov);
+                        }
                         return score;
                     }
                     alpha = score;
+                    improvedalpha = true;
                 }
                 pvtable[ply][ply + 1] = mov;
                 pvtable[ply][0] = pvtable[ply + 1][0] ? pvtable[ply + 1][0] : ply + 2;
@@ -173,6 +311,11 @@ int Searcher::alphabeta(int depth, int ply, int alpha, int beta, int play) {
                 }
             }
         }
+    }
+    int realnodetype = improvedalpha ? EXPECTED_PV_NODE : EXPECTED_ALL_NODE;
+    int savedmove = improvedalpha ? pvtable[ply][ply + 1] : ttmove;
+    if ((update || realnodetype == EXPECTED_PV_NODE) && !stopsearch) {
+        ttentry.update(currhash, depth, ply, bestscore, realnodetype, savedmove);
     }
     return bestscore;
 }
@@ -228,6 +371,9 @@ void Searcher::reset() {
         }
     }
     lastmove = 0;
+    for (int i = 0; i < TT_SIZE; i++) {
+        TT[i].data = (U64)0;
+    }
 }
 void Searcher::interface() {
     std::string ucicommand;
@@ -244,7 +390,8 @@ void Searcher::interface() {
     if (token == "deck") {
         std::string cards;
         tokens >> token;
-        tokens >> cards;
+        std::streampos pos = tokens.tellg();
+        cards = tokens.str().substr(pos);
         if (token == "engine") {
             decks.set(decks.color, cards);
         }
@@ -303,6 +450,7 @@ void Searcher::interface() {
     }
 }
 int main() {
+    initializezobrist();
     Searcher engin;
     engin.reset();
     while (true) {
